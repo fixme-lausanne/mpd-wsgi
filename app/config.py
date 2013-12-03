8c2629a41eb2dd3582b05db3ff779c7c869dca45
gcmalloc <gcmalloc@gmail.com>
2013-12-03 21:47:27 +0100

    including local settings for password and local configuration

--- app/config.py
+++ app/config.py
@@ -4,3 +4,8 @@ PASSWORD = None
 MPD_ROOT = "/media/disk1/music/"
 
 UPLOAD_DIR = "/media/disk1/share/incoming/incoming/music"
+
+try:
+    from local_settings import *
+except ImportError:
+    print("cannot import local settings")
