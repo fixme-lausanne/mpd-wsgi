d5bff940ff10f7592b3275c33dfc86522fb6b9aa
dgellow <samuel.elborai@gmail.com>
2014-07-21 00:21:50 +0200

    Update vagrant provisioning, add mpd config and some fixes

--- api/fixme_config.py
+++ api/fixme_config.py
@@ -1,13 +1,14 @@
-HOST = "mpd.local"
+HOST = "mpd.fixme.ch"
 PORT = 6600
 PASSWORD = None
-MPD_ROOT = "/var/lib/mpd/music"
+MPD_ROOT = "/media/disk1/music/"
 
-UPLOAD_DIR = "/var/lib/mpd/upload"
+UPLOAD_DIR = "/media/disk1/share/incoming/incoming/music"
 LIMIT_SEARCH = 50
 
 #config that should be locally changed
 LASTFM_KEY = ""
+
 
 try:
     from local_settings import *
--- config/server-config.sh
+++ config/server-config.sh
@@ -1,16 +1,21 @@
 # Dependencies
 MY_REPO="https://dl.dropboxusercontent.com/u/103118232/deb-packages/"
 ARCH="amd64"
-
-echo "deb $MY_REPO $ARCH/" | sudo tee -a /etc/apt/sources.list
+MPD_ROOT="/var/lib/mpd"
+MPD_MUSIC_DIR="$MPD_ROOT/music"
+MPD_UPLOAD_DIR="$MPD_ROOT/upload"
 
 echo "System update"
 sudo apt-get update -y
+sudo apt-get install -y apt-transport-https
+echo "deb $MY_REPO $ARCH/" | sudo tee -a /etc/apt/sources.list
+sudo apt-get update -y
+
 echo "Install packages"
 sudo apt-get install -y \
-    apt-transport-https \
-    subversion git \
+    subversion git wget \
     python-pip python python-dev \
+    mpd mpc \
     nginx
 
 # Python
@@ -31,7 +36,32 @@ npm install
 node node_modules/bower/bin/bower install
 node node_modules/gulp/bin/gulp.js build
 
+# Hosts
+echo "/etc/hosts configuration"
+HOST_FILE=/etc/hosts
+echo | sudo tee -a $HOST_FILE
+echo "# mpd-wsgi" | sudo tee -a $HOST_FILE
+echo "127.0.0.1 mpd.local" | sudo tee -a $HOST_FILE
+echo "127.0.0.1 api.mpd.local" | sudo tee -a $HOST_FILE
+echo "127.0.0.1 client.mpd.local" | sudo tee -a $HOST_FILE
+
+# MPD
+echo "MPD configuration"
+sudo mkdir $MPD_MUSIC_DIR $MPD_UPLOAD_DIR
+sudo chmod -R a+rw $MPD_ROOT
+
+echo "Get some music"
+cd $MPD_MUSIC_DIR
+wget -i /vagrant/config/sample_music.txt
+
+echo "Create MPD db"
+sudo service mpd start
+mpc update
+
 # Finish
+echo
+echo "_______________________________________________"
+echo
 echo "Run the api server with:"
 echo "    cd /vagrant/api"
 echo "    gunicorn -k flask_sockets.worker app:app"
