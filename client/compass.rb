d1b4b75fec4bf2411c0ca5dbe676bb87dd143ba6
dgellow <samuel.elborai@gmail.com>
2015-03-14 00:32:16 +0100

    remove client dependencies

--- client/compass.rb
+++
@@ -1,6 +0,0 @@
-css_dir = 'build'
-sass_dir = 'src/scss'
-images_dir = 'build/images'
-fonts_dir = 'build/fonts'
-relative_assets = true
-environment = :development
--- client/gulp/tasks/watch.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var fs = require('fs');
-var onlySaneFiles = require('../util/fileFilter');
-
-gulp.task('watch', ['setWatch', 'browserSync'], function() {
-    gulp.watch('src/sass/**', ['compass']);
-    gulp.watch('src/images/**', ['images']);
-    // Note: The browserify task handles js recompiling with watchify
-});
