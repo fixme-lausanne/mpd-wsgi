f5322f7eee2ca7e2bb9a54213d3e499975f3a7fd
dgellow <samuel.elborai@gmail.com>
2015-03-14 09:30:51 +0100

    use Link in navbar

--- client/src/components/nav_tabs.jsx
+++ client/src/components/nav_tabs.jsx
@@ -1,18 +1,19 @@
 /*global require,module*/
-var React = require('react');
+var React = require('react'),
+    Router = require('react-router'),
+    Link = Router.Link;
 
 var NavTabs = React.createClass({
     render: function() {
         return (
             <nav id="list-tabs">
-              <dl className="sub-nav">
-                <dt></dt>
-                <dd><a href="/#/songs">Songs</a></dd>
-                <dd><a href="/#/albums">Albums</a></dd>
-                <dd><a href="/#/artists">{this.props.name}</a></dd>
-                <dd><a href="/#/genres">Genres</a></dd>
-                <dd><a href="/#/playlists">Playlists</a></dd>
-              </dl>
+              <ul className="sub-nav">
+                <li><Link to="/songs">Songs</Link></li>
+                <li><Link to="albums">Albums</Link></li>
+                <li><Link to="/artists">Artists</Link></li>
+                <li><Link to="/genres">Genres</Link></li>
+                <li><Link to="playlists">Playlists</Link></li>
+              </ul>
             </nav>
         );
     }
