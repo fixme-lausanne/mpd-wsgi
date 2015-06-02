090ae6af0baa88ab9748ca27d615e2f0f2eec0ce
dgellow <samuel.elborai@gmail.com>
2015-06-02 14:57:00 +0200

    move client into a dedicated project
    Available here: https://github.com/fixme-lausanne/mpdwsgi-web

--- client/bin/jsx-preprocessor.js
+++
@@ -1,8 +0,0 @@
-// Transform JSX to js.
-// Used by Jest.
-var ReactTools = require('react-tools');
-module.exports = {
-    process: function(src, path) {
-        return ReactTools.transform(src);
-    }
-};
